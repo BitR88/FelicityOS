6b106536efcc6dd2da95ddb57fd236c41f13620c
HoopR25 <szaboadam0209@proton.me>
2024-07-17 18:05:36 +0200

    Started testing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
-#![feature(asm)]
 #![no_std]
 #![no_main]
 
+#![feature(asm)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#![reexport_test_harness_main = "test_main"]
+
+
 use core::panic::PanicInfo;
+use core::fmt;
+use core::fmt::Write;
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()])
+{
+    println!("Running {} tests.", tests.len());
+    for test in tests
+    {
+        test();
+    }
+}
+
+
+
+
 
 mod vga_buffer;
 
 
 #[panic_handler]
-fn panic (_info: &PanicInfo) -> ! {
-    println!("{}", _info);
+fn panic (info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop { }
 }
 
@@ -17,6 +40,19 @@ fn panic (_info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start () -> ! {
     println!("Hello World!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+    
     loop { }
 }
+
+
+#[test_case]
+fn trivial_assertion()
+{
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
